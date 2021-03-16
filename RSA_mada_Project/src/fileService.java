c89c86c9add57e3223e36fd6d6c489d3ccfc7aba
Sebastian Grau <sebastian.grau@students.fhnw.ch>
2021-03-16 21:56:53 +0100

    CLeand up code, added comments, changed fileService to class

--- RSA_mada_Project/src/fileService.java
+++ RSA_mada_Project/src/fileService.java
@@ -8,73 +8,58 @@ import java.util.Scanner;
 public class fileService implements Key {
     
     //-----------------------Attributes-----------------------
+    //Define files for secure and public key
     private static File skFile = new File("src/sk.txt");
     private static File pkFile = new File("src/pk.txt");
-    
-    
-    //----------------------Constructors----------------------
-    //TODO GRAU: (?) Why Object and not Class || Explain pls, i wanna know why
-    fileService() {
- 
-    }
-    
+
     //-------------------------Methods------------------------
     //TODO GRAU: Exception Handler for empty files -> should return empty String
-    //TODO GRAU: Code Structure, clean up the tabs :P
-    //TODO GRAU: Comments, even if obvious
+    //TODO GRAU: Make change path method 
     
-        public static String readSk() throws FileNotFoundException {
-            
-            
-            Scanner sc = new Scanner(skFile); 
-          
-            return sc.nextLine(); 
+    //Read secure key from file from sk.txt
+    public static String readSk() throws FileNotFoundException {
+        Scanner sc = new Scanner(skFile); 
+        return sc.nextLine(); 
+    }
+    
+    //Read public key from file pk.txt
+    public static String readPk() throws FileNotFoundException {
+            Scanner sc = new Scanner(pkFile); 
+            return sc.nextLine();  
         }
-        
-        public static String readPk() throws FileNotFoundException {
-                
-               
-                Scanner sc = new Scanner(pkFile); 
-              
-                return sc.nextLine();  
-            }
-        
-        public static String readFile(String fileName) throws FileNotFoundException {
-            
-            
-            Scanner sc = new Scanner(new File("src/" + fileName)); 
-          
-            return sc.nextLine(); 
-        }
+    
+    //Read file with given name
+    public static String readFile(String fileName) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/" + fileName)); 
+        return sc.nextLine(); 
+    }
+    
+    //Write secure key into sk.txt
+    public static boolean writeSk(String key) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(skFile));
+        bw.write(key);
+        bw.close();
 
-        public static boolean writeSk(String key) throws IOException {
-            
-            BufferedWriter bw = new BufferedWriter(new FileWriter(skFile));
-            bw.write(key);
-            bw.close();
-            
-            return true;
-        }
-        
-        public static boolean writePk(String key) throws IOException {
-                
-                BufferedWriter bw = new BufferedWriter(new FileWriter(pkFile));
-                bw.write(key);
-                bw.close();
-                
+        return true;
+    }
+    
+    //Write public key into pk.txt
+    public static boolean writePk(String key) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(pkFile));
+        bw.write(key);
+        bw.close();             
 
-                return true;
-            }
-        
-        public static boolean writeFile(String fileName, String text) throws IOException {
-            
-            BufferedWriter bw = new BufferedWriter(new FileWriter("src/" + fileName));
-            bw.write(text);
-            bw.close();
-            
-
-            return true;
+        return true;
         }
+    
+    //Write string into given file
+    public static boolean writeFile(String fileName, String text) throws IOException {            
+        BufferedWriter bw = new BufferedWriter(new FileWriter("src/" + fileName));
+        bw.write(text);
+        bw.close();          
+
+        return true;
+    }
         
        
    }
