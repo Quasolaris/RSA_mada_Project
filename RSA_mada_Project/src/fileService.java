6c10614843b886258a2b1b9d5015dabde472ca3b
Sebastian Grau <grau.sebastian@protonmail.ch>
2021-03-16 18:05:43 +0100

    Adding some general methods to fileservice

--- RSA_mada_Project/src/fileService.java
+++ RSA_mada_Project/src/fileService.java
@@ -5,14 +5,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class fileService {
+public class fileService implements Key {
     
+    //-----------------------Attributes-----------------------
     private File skFile = new File("src/sk.txt");
     private File pkFile = new File("src/pk.txt");
     
+    
+    //----------------------Constructors----------------------
     fileService() {
  
     }
+    
+    //-------------------------Methods------------------------
     
         public String readSk() throws FileNotFoundException {
             
@@ -30,26 +35,49 @@ public class fileService {
                 return sc.nextLine();  
             }
         
+        public String readFile(String fileName) throws FileNotFoundException {
+            
+            
+            Scanner sc = new Scanner(new File(fileName)); 
+          
+            return sc.nextLine(); 
+        }
+
         public boolean writeSk(String key) throws IOException {
             
-            FileWriter fr = new FileWriter(skFile);
-            BufferedWriter bw = new BufferedWriter(fr);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(skFile));
             bw.write(key);
             bw.close();
             
             return true;
         }
         
-         public boolean writePk(String key) throws IOException {
+        public boolean writePk(String key) throws IOException {
                 
-                FileWriter fr = new FileWriter(pkFile);
-                BufferedWriter bw = new BufferedWriter(fr);
+                BufferedWriter bw = new BufferedWriter(new FileWriter(pkFile));
                 bw.write(key);
                 bw.close();
                 
 
                 return true;
             }
+        
+        public boolean writeFile(String fileName, String text) throws IOException {
+            
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+            bw.write(text);
+            bw.close();
+            
+
+            return true;
+        }
+        
+        public int[] getKeyValues(String fileName) {
+            
+            int[] values = new int[2];
+            String key = readFile(fileName);
+            
+        }
    }
   
 
