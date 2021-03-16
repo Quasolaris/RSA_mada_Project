99920efa937e139cebae47213c9a97958d16a318
Adrian Roth <adrian.roth@students.fhnw.ch>
2021-03-16 14:53:02 +0100

    Added Key-Interface

---
+++ RSA_mada_Project/src/key.java
@@ -0,0 +1,5 @@
+interface key {
+	
+	//====================Methods====================
+	public String toString();
+}
--- RSA_mada_Project/src/main_RSA.java
+++ RSA_mada_Project/src/main_RSA.java
@@ -3,7 +3,7 @@ public class main_RSA {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        
     }
 
 }
